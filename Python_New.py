0c81998eec5c5785514d36aa27cff9ecca147e86
XavZol <alansolis707@gmail.com>
2026-04-07 23:04:40 -0600

    Arrays

--- Python_New.py
+++ Python_New.py
@@ -1,21 +1,27 @@
 import numpy as np
 import pandas as pd 
+import time 
 
-ruta = (r'C:\Users\javie\OneDrive\Desktop\Excel_DB\Ciudades.csv')
-df = pd.read_csv(ruta)
+mi_array = np.array([1, 2, 3, 4, 5])
+print(mi_array)
 
-mean_population = df['Población'].mean() # Calcular la media de la columna 'Población'
-print(f'{mean_population}')
+print(type(mi_array))
 
-mean_population = np.round(df['Población'].mean()) # Redondear el resultado a un número entero
-print(f'{mean_population}')
+lista_grande = list(range(1000000))
+array_grande = np.array(lista_grande)
 
-min_visitors = np.min(df['Visitantes']) # Calcular el valor mínimo de la columna 'Visitantes'
-print(f'{min_visitors}')
+print(type(lista_grande))
+print(type(array_grande))
 
-np.max(df['Visitantes']) # Calcular el valor máximo de la columna 'Visitantes'
-print(f'{np.max(df["Visitantes"])}')
+inicio_lista = time.time() # Inicia el tiempo para la lista
+for i in lista_grande: # Itera sobre cada elemento de la lista y lo eleva al cuadrado
+    i ** 2 # Eleva al cuadrado cada elemento de la lista
+fin_lista = time.time() # Finaliza el tiempo para la lista
+print("Tiempo lista: ", fin_lista - inicio_lista) # Imprime el tiempo que tomó iterar sobre la lista y elevar cada elemento al cuadrado
 
-print(dir(np)) # Mostrar los métodos disponibles en el módulo numpy
+inicio_array = time.time() # Inicia el tiempo para el array
+array_grande ** 2
+fin_array = time.time() # Finaliza el tiempo para el array
+print("Tiempo array: ", fin_array - inicio_array) # Imprime el tiempo que tomó elevar al cuadrado cada elemento del array
 
 

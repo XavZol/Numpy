913c1cf945c5b15e841ed5299d3bcdc176915c83
XavZol <alansolis707@gmail.com>
2026-04-11 22:03:12 -0600

    Arrays sin archivos.

--- Python_New.py
+++ Python_New.py
@@ -2,26 +2,15 @@ import numpy as np
 import pandas as pd 
 import time 
 
-mi_array = np.array([1, 2, 3, 4, 5])
-print(mi_array)
+array = np.array([1 , 2 , 3 ])
+print(len(array))
 
-print(type(mi_array))
+print(array.shape)
 
-lista_grande = list(range(1000000))
-array_grande = np.array(lista_grande)
+array_2d = np.array([[1, 2, 3], [4, 5, 6]])
 
-print(type(lista_grande))
-print(type(array_grande))
+print(array_2d)
 
-inicio_lista = time.time() # Inicia el tiempo para la lista
-for i in lista_grande: # Itera sobre cada elemento de la lista y lo eleva al cuadrado
-    i ** 2 # Eleva al cuadrado cada elemento de la lista
-fin_lista = time.time() # Finaliza el tiempo para la lista
-print("Tiempo lista: ", fin_lista - inicio_lista) # Imprime el tiempo que tomó iterar sobre la lista y elevar cada elemento al cuadrado
+print(len(array_2d))
 
-inicio_array = time.time() # Inicia el tiempo para el array
-array_grande ** 2
-fin_array = time.time() # Finaliza el tiempo para el array
-print("Tiempo array: ", fin_array - inicio_array) # Imprime el tiempo que tomó elevar al cuadrado cada elemento del array
-
-
+print(array_2d.shape) 

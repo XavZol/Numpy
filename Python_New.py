399de7b6523d368d1858b3af7db1a46a6713fb51
XavZol <alansolis707@gmail.com>
2026-04-13 21:36:59 -0600

    Indexación y Segmentación de Arrays

--- Python_New.py
+++ Python_New.py
@@ -2,33 +2,34 @@ import numpy as np
 import pandas as pd 
 import time 
 
+array1d = np.array([1, 2, 3, 4, 5])
 
-x = [ 1, 2, 3]
-y = [ 4, 5, 6]
-z = [ 7, 8, 9]
+array2d = np.array([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]])
 
-print(np.concat((x, y, z))) # concatenar arrays concat y concatenate hacen lo mismo, pero concat es mas rapido que concatenate
-print(np.concatenate((x, y, z)))
+print(array1d[0]) # ingresamos el indice del elemento que queremos mostrar, en este caso el primer elemento del array1d
 
-a = [[1, 2], 
-        [3, 4]]
-b = [[5, 6], 
-        [7, 8]]
+print(array1d[-1]) # ingresamos el indice del elemento que queremos mostrar, en este caso el ultimo elemento del array1d, el indice -1 hace referencia al ultimo elemento del array
 
-print(np.concat([a] + [b], axis=1))
-print(np.concat([a] + [b], axis=0)) # concatenar con axis=0 es lo mismo que sin axis, pero es mas rapido que con axis=0
+print(array2d[0]) # ingresamos el indice del elemento que queremos mostrar, en este caso el primer elemento del array2d, el resultado es un array de 3 elementos, ya que el primer elemento del array2d es un array de 3 elementos
 
-array_concat = np.concat([a] + [b], axis=0)
-print(np.shape(array_concat))
+print(array2d[0, 0]) # ingresamos el indice del elemento que queremos mostrar, en este caso el primer elemento del primer elemento del array2d, el resultado es un numero, ya que el primer elemento del primer elemento del array2d es un numero
 
-array_reformado = array_concat.reshape(2, 4) # reformar el array a una nueva forma, en este caso a una matriz de 2 filas y 4 columnas
-print(array_reformado)
+print(array2d[1, 2]) # ingresamos el indice del elemento que queremos mostrar, en este caso el tercer elemento del segundo elemento del array2d, el resultado es un numero, ya que el tercer elemento del segundo elemento del array2d es un numero
+print(array2d[1] [2]) # lo mismo que la linea anterior, pero con una sintaxis diferente, el resultado es el mismo, ya que el tercer elemento del segundo elemento del array2d es un numero
 
-array_sumado = array_reformado + array_reformado # sumar el array consigo mismo, lo que es lo mismo que multiplicar por 2, pero es mas rapido que multiplicar por 2
-print(array_sumado) 
 
-array_mult = array_reformado * array_reformado # multiplicar el array consigo mismo, lo que es lo mismo que elevar al cuadrado, pero es mas rapido que elevar al cuadrado
-print(array_mult)
+print(array1d[1:4]) # ingresamos el indice del elemento que queremos mostrar, en este caso los elementos desde el indice 1 hasta el indice 3 del array1d, el resultado es un array de 3 elementos, ya que los elementos desde el indice 1 hasta el indice 3 del array1d son 3 elementos
 
-raices = np.sqrt(array_mult) # calcular la raiz cuadrada del array, lo que es lo mismo que elevar al cuadrado y luego sacar la raiz cuadrada, pero es mas rapido que elevar al cuadrado y luego sacar la raiz cuadrada
-print(raices)
+print(array2d[1, :]) # ingresamos el indice del elemento que queremos mostrar, en este caso todos los elementos del segundo elemento del array2d, el resultado es un array de 3 elementos, ya que todos los elementos del segundo elemento del array2d son 3 elementos
+
+print(array2d[1, 1:3]) # ingresamos el indice del elemento que queremos mostrar, en este caso los elementos desde el indice 1 hasta el indice 2 del segundo elemento del array2d, el resultado es un array de 2 elementos, ya que los elementos desde el indice 1 hasta el indice 2 del segundo elemento del array2d son 2 elementos
+
+print(array2d[: , 1]) # ingresamos el indice del elemento que queremos mostrar, en este caso todos los elementos del segundo elemento de cada elemento del array2d, el resultado es un array de 3 elementos, ya que todos los elementos del segundo elemento de cada elemento del array2d son 3 elementos
+
+print(array2d[:2, :2]) # ingresamos el indice del elemento que queremos mostrar, en este caso los elementos desde el indice 0 hasta el indice 1 de cada elemento del array2d, el resultado es un array de 4 elementos, ya que los elementos desde el indice 0 hasta el indice 1 de cada elemento del array2d son 4 elementos
+
+print(array1d > 3) # ingresamos una condición para filtrar los elementos del array1d, en este caso los elementos mayores que 3, el resultado es un array de booleanos, ya que la comparación devuelve un valor booleano para cada elemento del array1d
+
+print(array2d % 2 == 0) # ingresamos una condición para filtrar los elementos del array2d, en este caso los elementos pares, el resultado es un array de booleanos, ya que la comparación devuelve un valor booleano para cada elemento del array2d

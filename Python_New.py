3dc63d83a0a5938d3fed73330b87fb96caa7f8e5
XavZol <alansolis707@gmail.com>
2026-04-13 22:02:31 -0600

    Operaciones Avanzadas y Funciones Universales.

--- Python_New.py
+++ Python_New.py
@@ -2,23 +2,37 @@ import numpy as np
 import pandas as pd 
 import time 
 
-array = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-print(array)
+array1 = np.array([1, 2, 3])
+print(array1)
 
-print(array.shape)
+array2 = np.array([[0], [10], [20], [30]])
+print(array2)
 
-array_mod = array.reshape(3, 3) #reshape the array to 3 rows and 3 columns
-print(array_mod)
+broadcast_suma = array2 + array1 # broadcasting, se suman cada elemento de array1 a cada fila de array2
+print(broadcast_suma)
 
-array_mod2 = array.reshape(-1, 3) #reshape the array to 3 columns and as many rows as needed (in this case, 3 rows)
-print(array_mod2)
+broadcast_mult = array2 * array1 # broadcasting, se multiplican cada elemento de array1 a cada fila de array2
+print(broadcast_mult)
 
-array_volteado = array_mod2.transpose() #transpose the array, which means to flip it over its diagonal. The rows become columns and the columns become rows.
-print(array_volteado)
+# Funciones Universales
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
 
-array_chat = array_mod.flatten() # flatten the array, which means to convert it into a 1D array. The shape of the array will be (9,) because it has 9 elements.
-print(array_chat)
+resultado = np.add(a, b) # suma elemento a elemento
+print(resultado)
 
-array_ravel = array_mod.ravel() # ravel the array, which means to convert it into a 1D array. The shape of the array will be (9,) because it has 9 elements. The difference between flatten and ravel is that flatten returns a copy of the array, while ravel returns a view of the array. This means that if you modify the ravelled array, it will also modify the original array, while if you modify the flattened array, it will not modify the original array.
-print(array_ravel)
+#substract, multiply, divide, power, log, exp, sin, cos, tan, etc
+
+resultado2 = np.exp(a)
+print(resultado2)
+
+resultado3 = np.log(a)
+print(resultado3)
+
+resultado4 = np.log10(a)
+print(resultado4)
+
+resultado5 = np.sqrt(a)
+print(resultado5)
+

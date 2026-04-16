da0df30276b44e1a1a73613e2e687845b63179af
XavZol <alansolis707@gmail.com>
2026-04-15 21:53:36 -0600

    Ejercicio Metereológico

--- Python_New.py
+++ Python_New.py
@@ -2,25 +2,84 @@ import numpy as np
 import pandas as pd 
 import time 
 
-df = pd.DataFrame({
-    'Impares' : [1, 3, 5],
-    'Pares' : [2, 4, 6]
-})
-
+ruta = r"C:\Users\javie\OneDrive\Desktop\Excel_DB\meteo.csv"
+df = pd.read_csv(ruta)
 print(df)
 
-array_dp1 = df.values # con el metodo values de un dataframe podemos convertirlo a un array de numpy, el resultado es un array bidimensional con los mismos datos que el dataframe pero sin los indices ni las columnas
-print(array_dp1)
+print(df.head())
+print(df.describe())
 
-arra_n2 = df.to_numpy() # otra forma de convertir un dataframe a un array de numpy, el resultado es el mismo que con el metodo values
-print(arra_n2)
+temperatura = df['Temperatura'].to_numpy(copy=True)
+precipitacion = df['Precipitación'].to_numpy(copy=True)
+humedad = df['Humedad'].to_numpy(copy=True)
 
-print(np.sqrt(df)) # con el metodo sqrt de numpy podemos calcular la raiz cuadrada de cada elemento del dataframe, el resultado es un nuevo dataframe con los mismos indices y columnas pero con los valores transformados
+print(temperatura)
 
-print(np.max(df)) # con el metodo max de numpy podemos calcular el valor maximo de cada columna del dataframe, el resultado es una serie con los mismos indices que el dataframe pero con los valores maximos de cada columna
+# Identificar los datos faltantes en los arrays, y reemplazarlos por el promedio de los valores del repectivo array
+temp_nulo = np.isnan(temperatura)
+precip_nulo = np.isnan(precipitacion)
+humedad_nulo = np.isnan(humedad)
 
-df_desde_np = pd.DataFrame(array_dp1) #creamos un nuevo dataframe a partir del array creado con el metodo values
-print(df_desde_np)
+print(temp_nulo)
+print(precip_nulo)
+print(humedad_nulo)
 
-df_desde_np = pd.DataFrame(array_dp1, columns=['Col1', 'Col2']) #creamos un nuevo dataframe a partir del array creado con el metodo values y le asignamos nombres a las columnas
-print(df_desde_np)
+# Identificar el promedio de cada array, 
+temp_promedio = np.nanmean(temperatura)
+precip_promedio = np.nanmean(precipitacion)
+humedad_promedio = np.nanmean(humedad)
+
+print(temp_promedio)
+print(precip_promedio)
+print(humedad_promedio)
+
+# Reemplazar los valores nulos con el promedio del array
+temperatura[temp_nulo] = temp_promedio 
+precipitacion[precip_nulo] = precip_promedio
+humedad[humedad_nulo] = humedad_promedio
+
+print(temperatura)
+print(precipitacion)
+print(humedad)
+
+# Esta otra forma es más eficiente ya que no modifica el array original, sino que crea uno nuevo con los valores corregidos
+temp_correg = np.where(np.isnan(temperatura), temp_promedio, temperatura)
+print(temp_correg)
+
+#temperatura promedio es lo mismo que temp_promedio, pero con el array corregido, es decir, sin los valores nulos
+temp_promedio = np.mean(temperatura)
+print(temp_promedio)
+
+# Calcular la suma total de la precipitación, utilizando el array corregido
+precipitacion_total = np.sum(precipitacion)
+print(precipitacion_total)
+
+# Máxima húmedad registrada
+humedad_maxima = np.max(humedad)
+print(humedad_maxima)
+
+# Máxima temperatura registrada
+temperatura_maxima = np.max(temperatura)
+print(temperatura_maxima)
+
+# Registro correspondiente a la temperatura máxima
+registro_temp_max = np.where(temperatura == temperatura_maxima)[0][0]
+print(registro_temp_max)
+
+# Fecha correspondiente al registro más caluroso
+fecha_mas_caluroso = df.iloc[registro_temp_max]['Fecha']
+print(fecha_mas_caluroso)
+
+
+# Fecha más fría
+fecha_mas_fria = df.iloc[np.where(temperatura == np.min(temperatura))[0][0]]['Fecha']
+print(fecha_mas_fria)
+
+# Exportar los resultados a un nuevo archivo CSV
+resultados = pd.DataFrame({
+    'Metrica': ['Temperatura Promedio', 'Precipitación Total', 'Húmedad Máxima', 'Día Más Caluroso', 'Día Más Frío'],
+    'Valor': [temp_promedio, precipitacion_total, humedad_maxima, fecha_mas_caluroso, fecha_mas_fria]
+})
+
+# Guardar los archivos en un nuevo CSV
+resultados.to_csv('Resultados análisis metereológico.csv', index=False)
